5ce51de9eb6116eb05316cfe8f4ed0e034d13d71
Filipe Larga <filipe.larga@premium-minds.com>
2022-09-04 22:58:13 +0100

    Add match command (with team autocomplete) that creates a thread

--- lib/commands/match.ts
+++ lib/commands/match.ts
@@ -21,26 +21,48 @@ export default {
   async execute(
     interaction: ChatInputCommandInteraction | AutocompleteInteraction
   ) {
+    // SlashCommand
     if (interaction.isChatInputCommand()) {
-      // Validation -> is in Thread
+      // Validation -> is not in Thread
       const isThread = interaction.channel?.isThread();
       if (isThread) {
-        throw new UserError('Cannot be inside a thread');
+        throw new UserError('Cannot use command /match inside a thread');
       }
 
-      await interaction.reply('ok buddy');
+      // Validation -> team exists
+      const teamName = interaction.options.getString('team', true);
+      const team = TEAMS.filter((team) => team.name === teamName);
+      if (team.length === 0) {
+        throw new UserError(
+          'Team does not exist.\nOnly teams from the Primeira Liga are supported for now'
+        );
+      }
+
+      // Create thread
+      const threadMessage = await interaction.reply({
+        content: `Starting a Match thread...\n${team[0].name}`,
+        fetchReply: true
+      });
+      threadMessage.startThread({
+        name: `Match thread: ${team[0].name}`,
+        autoArchiveDuration: 1440,
+        reason: `Match thread for ${team[0].name}`
+      });
       return;
     }
 
+    // Autocomplete
     if (interaction.isAutocomplete()) {
       const focusedValue = interaction.options.getFocused();
-      const choices = TEAMS.map((team) => team.name);
-      const filtered = choices.filter((choice) =>
-        choice.startsWith(focusedValue)
-      );
-      filtered.length = 25; //Discord only allows 25 options at a time
+      let choices = TEAMS.map((team) => team.name);
+      if (focusedValue) {
+        choices = choices.filter((choice) =>
+          choice.toLowerCase().startsWith(focusedValue.toLowerCase())
+        );
+      }
+      choices = choices.slice(0, 25); //Discord only allows 25 options at a time
       await interaction.respond(
-        filtered.map((choice) => ({ name: choice, value: choice }))
+        choices.map((choice) => ({ name: choice, value: choice }))
       );
     }
   }
